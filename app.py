bf2dc19ea3c5e2815d71eab273652248b12b3b5e
takako4848 <143687877+takako4848@users.noreply.github.com>
2023-09-01 10:14:22 +0900

    2commit

--- app.py
+++ app.py
@@ -28,70 +28,71 @@ link= 'https://search.sbisec.co.jp/v2/popwin/info/stock/pop6040_usequity_list.ht
 st.markdown(link)
 st.write('ティッカーシンボルについては上のリンク（SBI証券）をご参照ください。')
 
-df_stock = pandas_datareader.data.get_data_yahoo(stock_name, '2020-01-05')
-st.header(stock_name + "2020年1月５日から現在までの株価（USD）")
-st.write(df_stock)
+try:
+    df_stock = pandas_datareader.data.get_data_yahoo(stock_name, '2020-01-05')
+    st.header(stock_name + "2020年1月５日から現在までの株価（USD）")
+    st.write(df_stock)
 
-st.header(stock_name + "　終値と１４日間平均（USD）")
-df_stock['SMA'] = df_stock['Close'].rolling (window=14).mean()
-df_stock2 = df_stock[['Close','SMA']]
-st.line_chart(df_stock2)
+    st.header(stock_name + "　終値と１４日間平均（USD）")
+    df_stock['SMA'] = df_stock['Close'].rolling (window=14).mean()
+    df_stock2 = df_stock[['Close','SMA']]
+    st.line_chart(df_stock2)
 
-st.header(stock_name + "値動き（USD）")
-df_stock['change'] = (((df_stock['Close'] - df_stock['Open'])) / (df_stock['Open']) * 100)
-st.line_chart(df_stock['change'].tail(100))
+    st.header(stock_name + "値動き（USD）")
+    df_stock['change'] = (((df_stock['Close'] - df_stock['Open'])) / (df_stock['Open']) * 100)
+    st.line_chart(df_stock['change'].tail(100))
 
-fig = go.Figure(
-            data = [go.Candlestick(
-                x = df_stock.index,
-                open = df_stock['Open'],
-                high = df_stock['High'],
-                low = df_stock['Low'],
-                close = df_stock['Close'],
-                increasing_line_color = 'green',
-                decreasing_line_color = 'red',
+    fig = go.Figure(
+        data = [go.Candlestick(
+            x = df_stock.index,
+            open = df_stock['Open'],
+            high = df_stock['High'],
+            low = df_stock['Low'],
+            close = df_stock['Close'],
+            increasing_line_color = 'green',
+            decreasing_line_color = 'red',
             )
         ]
     )
-st.header(stock_name + "キャンドルスティック")
-st.plotly_chart(fig, use_column_width=True)
+    st.header(stock_name + "キャンドルスティック")
+    st.plotly_chart(fig, use_column_width=True)
 
-df_stock['label'] = df_stock['Close'].shift(-30)
+    df_stock['label'] = df_stock['Close'].shift(-30)
 
-st.header(stock_name + '1か月後を予測しよう（USD）')
-def stock_predict():
+    st.header(stock_name + '1か月後を予測しよう（USD）')
+    def stock_predict():
 
-    X = np.array(df_stock.drop(['label', 'SMA', 'change'], sxis=1))
-    X = sklearn.Preprocessing.scale(X)
-    predict_data = X[-30:]
-    X = X[:-30]
-    y = np.array(df_stock['label'])
-    y= y[:-30]
+        X = np.array(df_stock.drop(['label', 'SMA', 'change'], axis=1))
+        X = sklearn.preprocessing.scale(X)
+        predict_data = X[-30:]
+        X = X[:-30]
+        y = np.array(df_stock['label'])
+        y = y[:-30]
 
-    X_train, X_test, y_train,y_test = sklearn.model_selection.train_test_split(
-        X,y,test_size= 0.2)
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+            X,y,test_size= 0.2)
 
-    model = sklearn.linear_model.LinearReqression()
-    model.fit(X_train,y_train)
+        model = sklearn.linear_model.LinearRegression()
+        model.fit(X_train,y_train)
 
-    accuracy = model.score(X_test,y_test)
+        accuracy = model.score(X_test,y_test)
 
-    st.write(f'正解率は{round((accuracy) * 100, 1)}%です。')
+        st.write(f'正解率は{round((accuracy) * 100, 1)}%です。')
 
-    if accuracy > 0.75:
-        st.write('信頼度：高')
-    elif accuracy > 0.5:
-        st.write('信頼度：中')
-    else:
-        st.write('信頼度：低')
-    st.write('オレンジの線（Predict）が予測値です。')
+        if accuracy > 0.75:
+            st.write('信頼度：高')
+        elif accuracy > 0.5:
+            st.write('信頼度：中')
+        else:
+            st.write('信頼度：低')
+        st.write('オレンジの線（Predict）が予測値です。')
 
-    predicted_data = model.predict(predict_data)
-    df_stock['Predict'] = np.nan
-    last_date = df_stock.iloc[-1].name
-    one_day = 86400
-    next_unix = last_date.timestamp() + one_day
-
+        predicted_data = model.predict(predict_data)
+        df_stock['Predict'] = np.nan
+        last_date = df_stock.iloc[-1].name
+        one_day = 86400
+        next_unix = last_date.timestamp() + one_day
+        
         for data in predicted_data:
             next_date = datetime.datetime.fromtimestamp(next_unix)
             next_unix += one_day
@@ -103,8 +104,14 @@ def stock_predict():
         df_stock3 = df_stock[['Close', 'Predict']]
         st.line_chart(df_stock3)
 
-        if st.button('予測する'):
-            stock_predict()
+    if st.button('予測する'):
+        stock_predict()
+
+except:
+    st.error(
+        "エラーがおきているようです。"
+    )
+st.write('Copyright © 2021 Tomoyuki Yoshikawa.All Rights Reserved.')
 
 
 
